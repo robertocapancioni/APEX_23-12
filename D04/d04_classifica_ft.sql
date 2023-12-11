1e97c6d0b894e37cba5c456826b3ee2f78426482
Roberto Capancioni <roberto.capancioni@gmail.com>
2023-12-11 16:24:11 +0100

    Update d04_classifica_ft.sql

--- D04/d04_classifica_ft.sql
+++ D04/d04_classifica_ft.sql
@@ -4,7 +4,7 @@ select classifica,
        settore,
        revenue_2020,
        anno_inizio
-  from d03_classifica_ft;
+  from d04_classifica_ft;
   
 select classifica,
        nome,
@@ -12,7 +12,7 @@ select classifica,
        settore,
        revenue_2020,
        anno_inizio
-  from d03_classifica_ft
+  from d04_classifica_ft
  where nazione = 'Italy';
  
   select classifica,
@@ -21,32 +21,32 @@ select classifica,
          settore,
          revenue_2020,
          anno_inizio
-    from d03_classifica_ft
+    from d04_classifica_ft
    where nazione = 'Italy'
 order by classifica
    fetch next 3 rows only; 
 
    select nazione,
           count(*) as numero
-     from d03_classifica_ft
+     from d04_classifica_ft
  group by nazione;
  
     select nazione,
           count(*) as numero,
           sum(revenue_2020) as tot_revenue_2020
-     from d03_classifica_ft
+     from d04_classifica_ft
  group by nazione;
   
    select count(*) as numero,
           count(web) as numero_web,
           count(distinct nazione) numero_nazioni
-     from d03_classifica_ft;
+     from d04_classifica_ft;
 
     select nazione,
            count(*) as numero,
            sum(revenue_2020) as tot_revenue_2020,
            avg(revenue_2020) as media_revenue_2020
-      from d03_classifica_ft
+      from d04_classifica_ft
   group by nazione
   order by tot_revenue_2020 desc
 fetch next 3 rows only;
@@ -55,7 +55,7 @@ fetch next 3 rows only;
            count(*) as numero,
            sum(revenue_2020) as tot_revenue_2020,
            avg(revenue_2020) as media_revenue_2020
-      from d03_classifica_ft
+      from d04_classifica_ft
   group by nazione
   order by sum(revenue_2020) desc
 fetch next 3 rows only;
@@ -64,7 +64,7 @@ select nazione,
            count(*) as numero,
            sum(revenue_2020) as tot_revenue_2020,
            avg(revenue_2020) as media_revenue_2020
-      from d03_classifica_ft
+      from d04_classifica_ft
   group by nazione
     having sum(revenue_2020) >180000000
   order by tot_revenue_2020 desc;
@@ -75,7 +75,7 @@ select nazione,
            sum(revenue_2020)          as tot_revenue_2020,
            max(revenue_2020)          as max_revenue_2020,
            round(avg(revenue_2020),2) as media_revenue_2020
-      from d03_classifica_ft
+      from d04_classifica_ft
      where settore = 'Technology'
   group by nazione
     having sum(revenue_2020) >180000000
